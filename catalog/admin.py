b16ed38442792aec09eef94a963eb7ef36123fb4
root <root@DESKTOP-CAAHHPG.localdomain>
2018-12-29 16:12:56 +0900

    First

---
+++ catalog/admin.py
@@ -0,0 +1,50 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from catalog.models import Author, Genre, Book, BookInstance
+
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+# admin.site.register(Author)
+admin.site.register(Genre)
+
+# 저자란 하단에 저자의 책들이 인라인으로 나열 될 수 있도록
+
+class BookInline(admin.TabularInline):
+    model = Book
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+# 저자란 2
+    inlines = [BookInline]
+
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower' , 'due_back', 'id', 'imprint')
+    list_filter = ('status', 'due_back')
+    
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
